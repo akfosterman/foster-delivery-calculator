311b8e69ea9d72681873c9aae20beb1207400b45
akfosterman <kyle@fosterconstructionak.com>
2026-05-16 15:26:02 -0800

    Initialize React application with DeliveryCalculator

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import DeliveryCalculator from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <DeliveryCalculator />
+  </React.StrictMode>
+);
